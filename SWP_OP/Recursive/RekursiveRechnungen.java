cbe7ae2bbb59f95cea17f0117f620a2371f7fc34
Maximilian-Pumpfer <mpumpfer@tsn.at>
2021-11-09 13:14:48 +0100

    Update RekursiveRechnungen.java

--- SWP_OP/Recursive/RekursiveRechnungen.java
+++ SWP_OP/Recursive/RekursiveRechnungen.java
@@ -3,6 +3,7 @@ package SWP_OP.Recursive;
 import java.util.Scanner;
 
 public class RekursiveRechnungen {
+
     static Scanner s = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Soll eine Summe oder Potenz berechnet werden?");
@@ -17,8 +18,18 @@ public class RekursiveRechnungen {
         right = true;
         System.out.println("Anzahl:");
         int amount = Integer.parseInt(s.nextLine());
-        System.out.println("Iterativ: " + IterSum(amount));
-        System.out.println("Rekursiv: " + RecSum(amount));
+        long t1 = System.nanoTime();
+        System.out.print("Iterativ: " + IterSum(amount));
+        long t2 = System.nanoTime();
+        System.out.println("\tZeit: " + ((t2-t1)/1000) + "ms");
+        long t3 = System.nanoTime();
+        System.out.print("Rekursiv: " + RecSum(amount));
+        long t4 = System.nanoTime();
+        System.out.println("\tZeit: " + ((t4-t3)/1000) + "ms");
+        long t5 = System.nanoTime();
+        System.out.print("End-Rekursiv: " + EndRecSum(0, amount));
+        long t6 = System.nanoTime();
+        System.out.println("\tZeit: " + ((t6-t5)/1000) + "ms");
        }
        if(ans.equals("Potenz")){
            right = true;
@@ -26,7 +37,14 @@ public class RekursiveRechnungen {
         int num = Integer.parseInt(s.nextLine());
         System.out.println("Potenz:");
         int amount = Integer.parseInt(s.nextLine());
-        System.out.println(RecPow(num, amount));
+        long t1 = System.nanoTime();
+        System.out.print("Rekursiv: " + RecPow(num, amount));
+        long t2 = System.nanoTime();
+        System.out.println("\tZeit: " + ((t2-t1)/1000) + "ms");
+        long t3 = System.nanoTime();
+        System.out.print("End-Rekursiv: " + EndRecPow(num, num, amount));
+        long t4 = System.nanoTime();
+        System.out.println("\tZeit: " + ((t4-t3)/1000) + "ms");
        }
        if(right == false){
            System.out.println("Eingabe ist falsch, bitte erneut eingeben!");
@@ -36,15 +54,15 @@ public class RekursiveRechnungen {
 
     }
 
-public static int IterSum(int count){
-    int result = 0;
-    for(int i = 0; i < count; i++){
+public static long IterSum(long count){
+    long result = 0;
+    for(long i = 0; i < count; i++){
         result+=i;
     }
     return result;
 }
 
-public static int RecSum(int count){
+public static long RecSum(long count){
 
        if (count == 0){
         return count;
@@ -53,12 +71,35 @@ public static int RecSum(int count){
        }
 }
 
-public static double RecPow(int num, int pow){
+public static long EndRecSum(long result, long amount){
+    if(amount==0){
+        return result;
+    }else{
+        return EndRecSum(result+(amount-1), amount-1);
+    }
+}
+
+public static long IterPow(long num, long pow){
+    int result = 0;
+    for(int i=0; i<pow; i++){
+        result*=num;
+    }
+    return result;
+}
+
+public static long RecPow(long num, long pow){
     if (pow == 1){
         return num;
        }else{
             return num * RecPow(num, pow - 1);
        }
+}
+public static long EndRecPow(long num, long base, long pow) {
+    if(pow==1){
+        return num;
+    }else{
+        return EndRecPow(num*base, base, pow-1);
+    }
 }
 
 }
