c74f6ded019684a6da0087c9af96cbd398c7a9c2
Josef Strillinger <jstrillinger@tsn.at>
2022-06-20 21:01:27 +0200

    Flask and Rest

---
+++ Abschlussproject-CCDE&UC/Flask und Rest/flask_control.py
@@ -0,0 +1,28 @@
+import json
+from flask_restful import Api
+#from model import Question, getRandomQuestion, getData, Service, AllQuests, getQuests
+from rest import MusicInfo, Service
+from flask import Flask, render_template, session
+
+app = Flask(__name__)
+app.secret_key = "epjsmp2021/22"
+api = Api(app)
+
+@app.route('/')
+def start():
+    return render_template("startseite.html")
+
+@app.route('/songs')
+def showSongs():
+    return render_template("songs.html", songs = getSongs())
+
+@app.route('/panel')
+@app.route('/panel/<string:song>')
+def panel():
+    return render_template("startseite.html")
+
+api.add_resource(Service, "/rest/<int:id>")
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
