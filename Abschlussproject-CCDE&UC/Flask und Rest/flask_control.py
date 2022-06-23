9e29f82dc0e67cab0942de0ab8837bdd6f36677e
Josef Strillinger <jstrillinger@tsn.at>
2022-06-23 21:43:11 +0200

    mqtt and flask combination

--- Abschlussproject-CCDE&UC/Flask und Rest/flask_control.py
+++ Abschlussproject-CCDE&UC/Flask und Rest/flask_control.py
@@ -26,24 +26,39 @@ from paho import mqtt
 # limitations under the License.
 
 import time
+from urllib.parse import urlparse
 import paho.mqtt.client as paho
 from paho import mqtt
 
 # setting callbacks for different events to see if it works, print the message etc.
 def on_connect(client, userdata, flags, rc, properties=None):
     print("CONNACK received with code %s." % rc)
+    #client.publish("pro/music", payload=client._client_id.decode("utf-8") + "-getSongs", qos=1)    #Später wieder aktivieren, wenn getSongs geschrieben, damit man abspielen kann
+    
 
-# with this callback you can see if your publish was successful
-def on_publish(client, userdata, mid, properties=None):
-    print("mid: " + str(mid))
+# # with this callback you can see if your publish was successful
+# def on_publish(client, userdata, mid, properties=None):
+#     print("mid: " + str(mid))
 
-# print which topic was subscribed to
-def on_subscribe(client, userdata, mid, granted_qos, properties=None):
-    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+# # print which topic was subscribed to
+# def on_subscribe(client, userdata, mid, granted_qos, properties=None):
+#     print("Subscribed: " + str(mid) + " " + str(granted_qos))
 
 # print message, useful for checking if it was successful
+allSongs = []
+
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    received = msg.payload.decode("utf-8").split("-");
+    if(received[0] != "main"):
+        print(received[0] + ": " + received[1])
+        received[1].replace("[", "")
+        received[1].replace("]", "")        
+        received[1].replace("'", "")
+        allSongs = received[1].split(",")
+        for s in allSongs:
+            print(s)
+# def on_log(client,userdata,level,buff):
+#     print(buff)  
 
 # using MQTT version 5 here, for 3.1.1: MQTTv311, 3.1: MQTTv31
 # userdata is user defined data of any type, updated by user_data_set()
@@ -54,28 +69,26 @@ client.on_connect = on_connect
 # enable TLS for secure connection
 client.tls_set(tls_version=mqtt.client.ssl.PROTOCOL_TLS)
 # set username and password
-client.username_pw_set("Stingaaa", "Satch01#")
+client.username_pw_set("project", "wasd1234")
 # connect to HiveMQ Cloud on port 8883 (default for MQTT)
-client.connect("15d9dcdf33e3466799ebbd0151f866ee.s2.eu.hivemq.cloud", 8883)
+client.connect("cbe265c6cda342daa94ba67720ef1767.s2.eu.hivemq.cloud", 8883)
 
 # setting callbacks, use separate functions like above for better visibility
-client.on_subscribe = on_subscribe
+# client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.on_publish = on_publish
+# client.on_publish = on_publish
+# client.on_log = on_log
 
 # subscribe to all topics of encyclopedia by using the wildcard "#"
-client.subscribe("encyclopedia/#", qos=1)
-
+client.subscribe("pro/music", qos=1)
 # a single publish, this can also be done in loops, etc.
-client.publish("encyclopedia/temperature", payload="hot", qos=1)
 
-# loop_forever for simplicity, here you need to stop the loop manually
-# you can also use loop_start and loop_stop
-#client.loop_forever()
+#client.publish("pro/music", payload=client._client_id.decode("utf-8") + "-play-Everything_Black.mp3", qos=1)
 
-client.loop_start()
-time.sleep(1)
-client.loop_stop()
+#client.loop_start()
+#time.sleep(10)
+#client.loop_stop()
+client.loop_forever()
 
 app = Flask(__name__)
 app.secret_key = "epjsmp2021/22"
@@ -94,7 +107,7 @@ def showSongs():
 def play(name):
     print(name)
     client.loop_start()
-    client.publish("encyclopedia/temperature", payload=name, qos=1)
+    client.publish("pro/music", payload = client._client_id.decode("utf-8") + "-play-" + name, qos=1)
     client.loop_stop()
     mixer.init()
     mixer.music.load(name)
@@ -104,8 +117,8 @@ def play(name):
 @app.route('/stop')
 def stop():
     client.loop_start()
-    client.publish("encyclopedia/temperature", payload="stop", qos=1)
-    time.sleep(10)
+    client.publish("pro/music", payload = client._client_id.decode("utf-8") + "-" + "stop", qos=1)
+    #time.sleep(1)
     client.loop_stop()
     mixer.music.stop()
     return render_template("play.html")
@@ -113,7 +126,7 @@ def stop():
 @app.route('/pause')
 def pause():
     client.loop_start()
-    client.publish("encyclopedia/temperature", payload="pause", qos=1)
+    client.publish("pro/music", payload = client._client_id.decode("utf-8") + "-" + "pause", qos=1)
     client.loop_stop()
     mixer.music.pause()
     return render_template("play.html")
@@ -121,7 +134,7 @@ def pause():
 @app.route('/unpause')
 def unpause():
     client.loop_start()
-    client.publish("encyclopedia/temperature", payload="unpause", qos=1)
+    client.publish("pro/music", payload = client._client_id.decode("utf-8") + "-" + "unpause", qos=1)
     client.loop_stop()
     mixer.music.unpause()
     return render_template("play.html")  
